d98d34f9615bed4f9ff9a676ea14797e661d5985
Mukka Deepak <ramu92013@gmail.com>
2021-05-04 00:11:54 +0530

    (update):✨updated all the issues

--- src/Components/tweet.js
+++ src/Components/tweet.js
@@ -12,32 +12,74 @@ function Tweet() {
     month: "Jan",
     year: "2021",
     tweet: "Your tweet! ",
-    am_pm:'PM',
+    am_pm: "PM",
     proImage: images,
+    retweets: "500",
+    quotetweets: "964",
+    likes: "987",
   };
 
   const [fileds, SetFileds] = useState(data);
-  const [pro,setPro]=useState(false)
+  const [pro, setPro] = useState(false);
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     SetFileds({ ...fileds, [name]: value });
   };
-  const handleProfile=(e)=>{
-const file=e.target.files[0];
-//const name=e.target.name;
-SetFileds({ ...fileds, proImage: URL.createObjectURL(e.target.files[0]) });
-setPro(true)
-console.log(URL.createObjectURL(e.target.files[0]))
-  }
+  const generateRandom = () => {
+ //   console.log("hello")
+   let fretweets= Math.round(Math.random() * 90000 + 1)
+   let flikes= Math.round(Math.random() * 90000 + 1)
+   let fquotetweets= Math.round(Math.random() * 90000 + 1)
+   if(fretweets >=1000)
+   {
+     fretweets/=1000
+     fretweets=fretweets.toFixed(1)
+    
+   }
+   
+   if(flikes >=1000)
+   {
+
+     flikes/=1000
+     flikes=flikes.toFixed(1)
+   }
+   
+   if(fquotetweets >=1000)
+   {
+
+     fquotetweets/=1000
+     fquotetweets=fquotetweets.toFixed(1)
+   }
+   SetFileds({...fileds,likes:`${flikes}K`,retweets:`${fretweets}K`,quotetweets:`${fquotetweets}K`})
+   
+   
+
+
+  };
+
+ 
+  const handleProfile = (e) => {
+    const file = e.target.files[0];
+    //const name=e.target.name;
+    SetFileds({ ...fileds, proImage: URL.createObjectURL(e.target.files[0]) });
+    setPro(true);
+    console.log(URL.createObjectURL(e.target.files[0]));
+  };
   return (
     <div className="tweet-main">
-      <div className="border-tweet">
+    
+        <div className="border-tweet">
         <div className="headerPart">
           <img src={fileds.proImage || images} />
           <div className="usernames" style={{ marginTop: "5px" }}>
             <span
-              style={{ marginLeft: "10px", color: "black", fontWeight: "bold" ,wordBreak:'break-word' }}
+              style={{
+                marginLeft: "10px",
+                color: "black",
+                fontWeight: "bold",
+                wordBreak: "break-word",
+              }}
             >
               {fileds.firsname}{" "}
               {remove && (
@@ -49,7 +91,11 @@ console.log(URL.createObjectURL(e.target.files[0]))
                     top: "2px",
                   }}
                 >
-                  <svg viewBox="0 0 24 24" style={{ height: "1rem" }}>
+                  <svg
+                    viewBox="0 0 24 24"
+                    style={{ height: "1rem" }}
+                    fill="rgba(29,161,242,1.00)"
+                  >
                     <g>
                       <path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path>
                     </g>
@@ -59,7 +105,13 @@ console.log(URL.createObjectURL(e.target.files[0]))
             </span>
 
             <br />
-            <span style={{ marginLeft: "10px", color: "#657786" ,wordBreak:'break-word'}}>
+            <span
+              style={{
+                marginLeft: "10px",
+                color: "#657786",
+                wordBreak: "break-word",
+              }}
+            >
               @{fileds.username}
             </span>
           </div>
@@ -76,13 +128,14 @@ console.log(URL.createObjectURL(e.target.files[0]))
           {fileds.tweet}
         </h3>
         <p style={{ color: "#657786" }}>
-          {fileds.time} {fileds.am_pm} ·{` ${fileds.month} ${fileds.date} , ${fileds.year}`}
+          {fileds.time} {fileds.am_pm} ·
+          {` ${fileds.month} ${fileds.date} , ${fileds.year}`}
         </p>
 
         <div className="numbers">
-          <span>5.5K </span> Retweets
-          <span style={{ marginLeft: "30px" }}>5.5K </span> Quotetweets
-          <span style={{ marginLeft: "30px" }}>5.5K </span> Likes
+          <span>{fileds.retweets}</span> Retweets
+          <span style={{ marginLeft: "30px" }}>{fileds.quotetweets} </span> Quote Tweets
+          <span style={{ marginLeft: "30px" }}>{fileds.likes} </span> Likes
         </div>
 
         <div className="emojis">
@@ -108,7 +161,16 @@ console.log(URL.createObjectURL(e.target.files[0]))
             </g>
           </svg>
         </div>
+
       </div>
+    <div style={{position:'absolute',left:'38%'}}>
+      <br/>
+     <span style={{display:'inline-flex'}}> <h4>Generate random retweets , likes , quotetweets</h4> <button className="generateButton" 
+     onClick={generateRandom}
+     >🗘</button></span>  
+    </div>
+     
+    
       <div className="container">
         <h5>Add verified tag</h5>
         <input
@@ -130,20 +192,23 @@ console.log(URL.createObjectURL(e.target.files[0]))
           onChange={(e) => handleChange(e)}
         />
         <h3>Time</h3>
-        <div style={{display:'inline-flex'}}>
-        <input
-          placeholder="Enter time (MM:SS)"
-          name="time"
-          onChange={(e) => handleChange(e)}
-        />
-         <select onChange={(e) => handleChange(e)} name="am_pm" style={{marginLeft:'5px',border:'1px solid #007BFF'}}>
-          <option selected="" disabled>
-            AM / PM
-          </option>
-          <option value="AM">AM</option>
-          <option value="PM">PM</option>
-         
-        </select>
+        <div style={{ display: "inline-flex" }}>
+          <input
+            placeholder="Enter time (MM:SS)"
+            name="time"
+            onChange={(e) => handleChange(e)}
+          />
+          <select
+            onChange={(e) => handleChange(e)}
+            name="am_pm"
+            style={{ marginLeft: "5px", border: "1px solid #007BFF" }}
+          >
+            <option selected="" disabled>
+              AM / PM
+            </option>
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
+          </select>
         </div>
         <h3>Date</h3>
         <input
@@ -152,11 +217,11 @@ console.log(URL.createObjectURL(e.target.files[0]))
           type="number"
           min="1"
           max="31"
-          style={{width:'100px'}}
+          style={{ width: "100px" }}
           onChange={(e) => handleChange(e)}
         />
         <h3>Month</h3>
-    
+
         <select onChange={(e) => handleChange(e)} name="month">
           <option selected="" disabled>
             Month
@@ -181,20 +246,26 @@ console.log(URL.createObjectURL(e.target.files[0]))
           onChange={(e) => handleChange(e)}
         />
         <h3>Tweet</h3>
-        <textarea rows="10" cols="50" style={{resize:'none',outline:'none',border:'1px solid #007BFF'}}
+        <textarea
+          rows="10"
+          cols="50"
+          style={{
+            resize: "none",
+            outline: "none",
+            border: "1px solid #007BFF",
+          }}
           placeholder="Enter tweet"
           name="tweet"
           onChange={(e) => handleChange(e)}
         />
         <h3>Profile picture</h3>
-        <input
-          type="file"
-          name="proImage"
-          onChange={(e) => handleProfile(e)}
-        />
+        <input type="file" name="proImage" onChange={(e) => handleProfile(e)} />
+        <br />
+    
         <br />
         <br/>
       </div>
+
     </div>
   );
 }
